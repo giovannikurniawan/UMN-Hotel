2bc600c96bae30b265c58453fdea3fa910d11395
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2021-05-20 17:52:50 +0700

    Initial Commit

---
+++ admin/view/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- <title>Document</title> -->
+	<!-- <link rel="stylesheet" href="css/style.css"> -->
+</head>
+<body>
+	<footer>
+		<h6>Copyright &copy; 2020 | Riechie , Giovanni Kurniawan & Muhammad Jual Akmal</h6>
+	</footer>
+</body>
+</html>
